e25d67b53e0641c1835934fea01517a3916a97f8
Cristian López <cristianlopezr.931@gmail.com>
2023-07-04 21:28:44 -0400

    New tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Pressable } from 'react-native';
 
 import tweets from '../../assets/data/tweets';
 import { Tweet } from '../../components/Tweet';
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
     return (
         <View style={styles.page}>
             <FlatList data={tweets} renderItem={({ item }) => <Tweet tweet={item} />} />
+            <Link href='new-tweet' asChild>
+                <Pressable style={styles.floatingButton}>
+                    <Entypo name='plus' size={24} color='white' />
+                </Pressable>
+            </Link>
         </View>
     );
 }
@@ -15,5 +22,24 @@ const styles = StyleSheet.create({
     page: {
         flex: 1,
         backgroundColor: 'white',
+    },
+    floatingButton: {
+        backgroundColor: '#1C9BF0',
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 15,
+        bottom: 15,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
 });
---
+++ app/new-tweet.tsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { Link, useRouter } from 'expo-router';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TextInput,
+    Pressable,
+    SafeAreaView,
+    StatusBar,
+    Platform,
+} from 'react-native';
+
+const user = {
+    id: 'u1',
+    username: 'VadimNotJustDev',
+    name: 'Vadim',
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+};
+
+function NewTweet() {
+    const [text, setText] = useState('');
+    const router = useRouter();
+
+    const onTweetPress = () => {
+        console.warn('Posting Tweet', text);
+
+        setText('');
+        router.back();
+    };
+
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Link href='../' style={{ fontSize: 18 }}>
+                        Cancel
+                    </Link>
+                    <Pressable style={styles.button} onPress={onTweetPress}>
+                        <Text style={styles.buttonText}>Tweet</Text>
+                    </Pressable>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Image src={user.image} style={styles.image} />
+                    <TextInput
+                        value={text}
+                        onChangeText={setText}
+                        style={{ flex: 1 }}
+                        placeholder="What's happening?"
+                        multiline
+                        numberOfLines={5}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default NewTweet;
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        backgroundColor: 'white',
+    },
+    container: {
+        padding: 10,
+        flex: 1,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#1C9BF0',
+        padding: 10,
+        paddingHorizontal: 20,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 50,
+        marginRight: 10,
+    },
+});
